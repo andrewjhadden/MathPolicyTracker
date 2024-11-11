e6aa66670fd634822acdb1731d1f947f68f3a3f2
andrewhadden5 <andrewhadden5@gmail.com>
2024-11-10 20:24:37 -0500

    added test print in data.js

--- Database/api/data.js
+++ Database/api/data.js
@@ -44,11 +44,10 @@ export default async function handler(req, res) {
 
     try {
         await connectToDB();
-        const data = await DataModel.find();  // Fetch data from MongoDB
+        const data = await DataModel.findOne();  // Fetch only one document
         res.status(200).json(data);
     } catch (error) {
         console.error('Error in handler:', error);
         res.status(500).json({ error: 'Internal Server Error', details: error.message });
     }
 }
-
