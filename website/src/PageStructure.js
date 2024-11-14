c73a2c73eece81cce9bff016f5c4262ce6e22266
allyberkowitz <98172374+allyberkowitz@users.noreply.github.com>
2024-11-14 13:23:46 -0500

    CSS main page 4

--- website/src/PageStructure.js
+++ website/src/PageStructure.js
@@ -68,8 +68,10 @@ function StructureWebsiteWithData() {
                                     <div className="title">Math-Related Congressional Bill Tracker</div>
                                 </div>
                                 <h1 className="alert-header">See What’s New!</h1>
-                                <div className="bill-alerts">
-                                    <BillAlerts data={data} />
+                                <div className="bill-alerts-wrapper">
+                                    <div className="bill-alerts">
+                                        <BillAlerts data={data} />
+                                    </div>
                                 </div>
                                 <h2 className="table-header">10 Most Recent Bill Actions</h2>
                                 <div className="search-bar">
