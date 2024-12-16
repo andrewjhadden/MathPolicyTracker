5ae590d4c85d5460d92c9a55812a6b8df2664d8d
andrewhadden5 <andrewhadden5@gmail.com>
2024-12-16 15:04:40 -0500

    Removed Dead Code

--- website/src/PageStructure.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import PageStructure from './PageStructure';
-
-test('renders learn react link', () => {
-    render(<PageStructure />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-});
